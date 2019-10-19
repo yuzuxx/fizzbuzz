a0b43f896e123ceeec2e6b87d6e1d23e8e4359c2
yu <yuzu.childmaster@gmail.com>
2019-10-19 22:29:48 +0800

    edit it should return...

--- test/fuzzbuzz_spec.js
+++ test/fuzzbuzz_spec.js
@@ -1,18 +1,18 @@
 var should = chai.should()
 describe('function fuzzbuzz', function () {
-  it('num can be divisible by 3 return Fizz', function () {
+  it('should return Fizznum when num can be divisible by 3 ', function () {
     var result = fizzBuzz(9)
     result.should.be.equal('Fizz')
   })
-  it('num can be divisible by 5 return Bizz', function () {
+  it('should return Bizznum when num can be divisible by 5 ', function () {
     var result = fizzBuzz(10)
     result.should.be.equal('Buzz')
   })
-  it('num can be divisible by 3 and 5 return FizzBizz', function () {
+  it('should return FizzBizz when num can be divisible by 3 and 5 ', function () {
     var result = fizzBuzz(15)
     result.should.be.equal('FizzBuzz')
   })
-  it('num cant be divisible by 3 or 5 return num', function () {
+  it('should return num when num cant be divisible by 3 or 5 ', function () {
     var result = fizzBuzz(11)
     result.should.be.equal(11)
   })
